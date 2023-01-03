41a5b02d4ddb92c485a67a72032c8aaf6786b5cb
YaelBadillo <yael.badillos@gmail.com>
2023-01-02 20:20:38 -0600

    Feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { Dashboard, Home, LogIn, SignUp } from './pages'
 
-
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/login" element={<LogIn />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+      <div className="h-full w-full">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth/signup" element={<SignUp />} />
+          <Route path="/auth/login" element={<LogIn />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   )
 }
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,6 +1,55 @@
+import { useFormik } from 'formik'
+
+import { Input, Button, Form, TextSuggestionLink } from '../components/form'
+import { TextSuggestion } from '../components/form/TextSuggestion';
+
 const SignUp = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      password: '',
+      passwordConfirm: '',
+    },
+    onSubmit: (values) => console.log(values),
+  })
+
   return (
-    <div>SignUp</div>
+    <div className="grid h-full w-full content-center bg-secondary">
+      <Form>
+        <Input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={formik.values.name}
+          onChange={formik.handleChange}
+          startIcon="badge"
+        />
+        <Input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          startIcon="lock"
+        />
+        <Input
+          type="password"
+          name="passwordConfirm"
+          placeholder="Confirm password"
+          value={formik.values.passwordConfirm}
+          onChange={formik.handleChange}
+          startIcon="enhanced_encryption"
+        />
+        <Button content="Continue" />
+
+        <TextSuggestion>
+          Already have an account?{' '}
+          <TextSuggestionLink>
+            Log in 
+          </TextSuggestionLink>
+        </TextSuggestion>
+      </Form>
+    </div>
   )
 }
 
