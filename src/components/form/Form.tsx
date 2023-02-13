9be7b871c070962d3b5574a5e0083dd11754c554
YaelBadillo <yael.badillos@gmail.com>
2023-02-13 00:24:36 -0600

    Refact: refact form

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -14,13 +14,13 @@ export const Form: FC<FormProps> = ({
   children,
 }: FormProps) => {
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="m-auto grid w-4/12 justify-items-center gap-3 rounded-3xl bg-primary px-6 py-6"
-    >
+    <div className="m-auto grid w-4/12 justify-items-center gap-3 rounded-3xl bg-primary px-6 py-6">
       <h2 className="text-2xl font-bold text-gray-light">{title}</h2>
       <p className="mb-2 text-sm text-gray-light">{text}</p>
-      <div className="grid w-full grid-cols-1 gap-y-3">{children}</div>
-    </form>
+
+      <form onSubmit={handleSubmit} className="grid w-full grid-cols-1 gap-y-3">
+        {children}
+      </form>
+    </div>
   )
 }
