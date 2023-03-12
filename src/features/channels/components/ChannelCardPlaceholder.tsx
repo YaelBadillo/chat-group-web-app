d77a11ae59820abfe7708c49877acfc5b72d4c5e
Yael Badillo <yael.badillos@gmail.com>
2023-03-12 16:46:50 -0600

    Styles: add padding to the channel card placeholder

--- src/features/channels/components/ChannelCardPlaceholder.tsx
+++ src/features/channels/components/ChannelCardPlaceholder.tsx
@@ -1,6 +1,6 @@
 export const ChannelCardPlaceholder = () => {
   return (
-    <div className="flex h-max w-full items-center gap-x-3">
+    <div className="flex h-max w-full items-center gap-x-3 p-1">
       <div className="h-11 w-11 animate-skeleton-loading rounded-lg bg-tertiary p-2 text-center text-base font-semibold leading-7 opacity-70"></div>
       <div className="h-11 w-full animate-skeleton-loading rounded-lg bg-tertiary opacity-70"></div>
     </div>
