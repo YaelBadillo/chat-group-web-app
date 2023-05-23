4a4656fc88b226d5e4699c277931beab58670714
Yael Badillo <yael.badillos@gmail.com>
2023-05-22 22:24:33 -0600

    Refact: refact dashboard elements props declaration

--- src/features/dashboard/layouts/LocationContainer.tsx
+++ src/features/dashboard/layouts/LocationContainer.tsx
@@ -4,9 +4,7 @@ interface LocationContainerProps {
   children: JSX.Element | JSX.Element[] | null
 }
 
-export const LocationContainer: FC<LocationContainerProps> = ({
-  children,
-}: LocationContainerProps) => {
+export const LocationContainer: FC<LocationContainerProps> = ({ children }) => {
   return (
     <div className="flex items-center px-6 pb-4 pt-6 text-gray-light shadow-line">
       {children}
