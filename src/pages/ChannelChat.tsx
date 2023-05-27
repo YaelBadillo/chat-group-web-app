90c397e881a4206db0ec6cea20ba68052d593b4b
Yael Badillo <yael.badillos@gmail.com>
2023-05-27 00:42:16 -0600

    Feat: add channel chat basic html structure and css

--- src/pages/ChannelChat.tsx
+++ src/pages/ChannelChat.tsx
@@ -2,8 +2,8 @@ import { useContext, useEffect } from 'react'
 
 import { useLocation, useParams } from 'react-router-dom'
 
-import { ChannelChatContainer } from '../features/channel'
 import { DashboardContext } from '../features/dashboard'
+import pet from '../assets/pet.jpg'
 
 export const ChannelChat = () => {
   const { channelId } = useParams()
@@ -11,9 +11,78 @@ export const ChannelChat = () => {
   const dashboard = useContext(DashboardContext)
 
   useEffect(() => console.log(location), [])
+
   return (
     <div className="flex flex-1 flex-col">
-      <div className="flex-1"></div>
+      <div className="flex flex-1 flex-col-reverse gap-y-9 overflow-y-scroll px-20 py-12">
+        <div className="flex w-10/12 flex-row gap-x-7">
+          <div className="h-10 w-10 flex-shrink-0 overflow-hidden rounded-md">
+            <img src={pet} alt="" className="h-full w-auto" />
+          </div>
+          <div>
+            <div className="flex items-baseline gap-x-6">
+              <span className="text-lg font-bold text-gray-dark">
+                Lorem, ipsum.
+              </span>
+              <span className="text-sm font-medium text-gray-dark">
+                today at 1:30 PM
+              </span>
+            </div>
+            <span className="text-lg font-medium text-gray-light">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus
+              fugiat distinctio debitis unde, error commodi facere blanditiis
+              quis quaerat laudantium? Nemo iusto explicabo dignissimos, dolorem
+              ipsam harum provident voluptate facere!
+            </span>
+          </div>
+        </div>
+
+        <div className="flex w-10/12 flex-row gap-x-7 self-end">
+          <div>
+            <div className="flex items-baseline justify-end gap-x-6">
+              <span className="text-lg font-bold text-gray-dark">
+                Lorem, ipsum dolor.
+              </span>
+              <span className="text-sm font-medium text-gray-dark">
+                today at 1:30 PM
+              </span>
+            </div>
+            <span className="text-lg font-medium text-gray-light">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus
+              fugiat distinctio debitis unde, error commodi facere blanditiis
+              quis quaerat laudantium? Nemo iusto explicabo dignissimos, dolorem
+              ipsam harum provident voluptate facere!
+            </span>
+          </div>
+          <div className="h-10 w-10 flex-shrink-0 overflow-hidden rounded-md">
+            <img src={pet} alt="" className="h-full w-auto" />
+          </div>
+        </div>
+
+        {/*
+        <div className="flex w-10/12 flex-row gap-x-7">
+          <div className="h-10 w-10 flex-shrink-0 overflow-hidden rounded-md">
+            <img src={pet} alt="" className="h-full w-auto" />
+          </div>
+          <div>
+            <div className="flex items-baseline gap-x-6">
+              <span className="text-lg font-bold text-gray-dark">
+                Lorem, ipsum dolor.
+              </span>
+              <span className="text-sm font-medium text-gray-dark">
+                today at 1:30 PM
+              </span>
+            </div>
+            <span className="text-lg font-medium text-gray-light">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus
+              fugiat distinctio debitis unde, error commodi facere blanditiis
+              quis quaerat laudantium? Nemo iusto explicabo dignissimos, dolorem
+              ipsam harum provident voluptate facere!
+            </span>
+          </div>
+        </div>
+  */}
+      </div>
       <div className="flex flex-row px-20 py-10">
         <form
           action=""
